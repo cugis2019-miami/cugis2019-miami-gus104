ac00f71c5e59b2a285f89017655910aaefd302f6
gus104 <52931989+gus104@users.noreply.github.com>
2019-07-15 15:51:58 -0400

    Add files via upload

---
+++ Gustavo Osorio.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 15 14:11:31 2019
+
+@author: admin
+"""
+
+5/8 #division
+
+print("hello world")
+
+print("53")
+
+53
+
+"hello world"
+
+print(54)
+
+
+(8/9)*3
+
+print("The division of 5/3 is", 5/3)
+
+#create function
+
+def plus(a,b):
+    plus = a + b
+    print(plus)
+    
+plus(2,3)
+
+#create function
+
+def areat(b,h):
+    areat = (1/2)*b*h
+    print(areat)
+    
+areat(2,6)
+
+#create function
+
+def areacircle(r):
+    areacircle = (3.14159)*r**2
+    print("The area of a circle with radius", r, "is", areacircle)
+    
+areacircle(4)
