6a014d9026c8e6a2a951cb8791947d0776e48f27
gus104 <52931989+gus104@users.noreply.github.com>
2019-07-16 16:01:01 -0400

    Add files via upload

---
+++ day 2 Gustavo Osorio.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 10:48:45 2019
+
+@author: admin
+"""
+
+#day 2
+
+def areasquare(a):
+    areasquare = a**2
+    print(areasquare)
+    
+areasquare(4)
+
+box = 5
+print("There are", box,"bars in the box")
+
+
+d = "Dark Chocolate"
+m = "Milk Chocolate"
+w = "White Chocolate"
+
+print("There are 3 types of chocolate in the box:")
+print(d)
+print(m)
+print(w)
+
+
+
+
+
+name = "Leroy."
+
+print("Good afternoon,", name,"How are you doing in the program so far?")
+
+
+
+import math
+
+dir(math)
+
+math.cos(8)
+
+
+math.remainder(12,7)
+math.sqrt(10)
+math.factorial(0)
+
+math.pow(2,4)
+math.pow(2,1/4)
+
+math.pow(6,8)
+math.pow(0,0)
+
+math.remainder(5,3)
+math.remainder(9,7)
+
+
+dir(math)
+
+
+
+# The Cubic Root of a Value
+
+def cubicroot(value):
+    cubicroot = math.pow(value,1/3)
+    print("The cubic root of the area of this computer screen,", value,"inches, is", cubicroot,"inches cubed.")
+
+cubicroot(45)
+
+
+def cube(y):
+    cube = math.pow
+
+
+
+
+# The Cubic Root of a Value that a User inserted
+
+print("Please enter a value to the variable t.")
+t = 8
+
+
+
+
+print("Enter a value for x")
+value = input("x: ")
+value = float(value)
+
+cube = 1/3
+
+answer = math.pow(value,cube)
+
+print("Answer:", answer)
+print("Now enter for t")
+t = input("t: ")
+t= float(t)
+
+square = 1/2
+
+newanswer = math.pow(t,square)
+    
+print("Answer:", newanswer)
+
+
+import math
+print("Please enter a value")
+y = 20
+    
+def cube(y):
+    cube = math.pow(y,1/3)
+    print("The cube root of", y,"is", cube)
+
+cube(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
