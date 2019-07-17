26a8dfd38de92c2343600838317207444c1cd654
gus104 <52931989+gus104@users.noreply.github.com>
2019-07-17 16:11:03 -0400

    Add files via upload

---
+++ day 3 Gustavo Osorio.py
@@ -0,0 +1,39 @@
+m = 5
+d = 3
+w = 8
+
+milk = "Milk Chocolate"
+dark = "Dark Chocolate"
+white = "White Chocolate"
+
+print("In the box there are", m, milk,"bars,", d, dark,"bars, and", w, white,"bars.")
+
+
+
+
+print("To find out how many bars of chocolate are in the box, answer the following questions:")
+m = input("How many bars of Milk Chocolate are there:")
+m = float(m)
+
+d = input("Now how many bars of Dark Chocolate are there: ")
+d = float(d)
+
+w = input("And how many bars of White Chocolate are there: ")
+w = float(w)
+
+print("In the box, there are", m+d+w,"bars in the box!")
+
+
+
+
+#dict data structure
+chocolate1 = {"cadburymilk":5}
+chocolate2 = {"cadburydark":3}
+chocolate3 = {"cadburywhite":8}
+#or
+chocolates = {"cadburymilk":5,"cadburydark":3,"cadburywhite":8}
+print(chocolates)
+
+
+
+
